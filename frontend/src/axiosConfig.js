2b8165d079a7dcd394be3ccbfeed75b20f082146
Ishan K <ishank@Ishans-MacBook-Air.local>
2026-02-15 15:27:40 +0530

    Updated axios baseURL for deployment

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5001' : '/';
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' 
+  ? 'https://bytes-bites-backend.onrender.com' 
+  : 'http://localhost:5001';
